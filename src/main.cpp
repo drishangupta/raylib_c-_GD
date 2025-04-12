0d9e10a5084ba93cfb7a9d38156f881a6ba8a198
Drishan Gupta <drishan2005@gmail.com>
2025-04-12 17:11:34 +0530

    Snake

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,42 @@
+#include <iostream>
 #include <raylib.h>
+#include <deque>
+#include <raymath.h>
 
 Color background = {173,204,96,255};
 Color snake = {43,51,24,255};
 
 int cellSize=30;
 int cellCount=25; 
+
+double lastUpdateTime=0;
+
+class Snake
+{
+
+    public:
+        std::deque<Vector2> body = {Vector2{6,9},Vector2{5,9},Vector2{4,9}};
+
+        Vector2 direction ={1,0};
+        void Draw()
+        {
+            for(unsigned int i = 0; i < body.size();i++)
+            {
+                float x=body[i].x;
+                float y=body[i].y;
+                Rectangle segment = Rectangle{x*cellSize, y*cellSize, (float)cellSize, (float)cellSize};
+                DrawRectangleRounded(segment,0.5,6,snake);
+
+            }
+
+        }
+        void update()
+        {
+            body.pop_back();
+            body.push_front(Vector2Add(body[0], direction));
+        }
+
+};
 
 class Food
 {
@@ -43,24 +75,25 @@ public:
 
 int main() 
 {
-    int ballX=400;
-    int ballY=400;
     
     InitWindow(cellSize*cellCount,cellSize*cellCount, "Retro Snake");
     SetTargetFPS(60);
 
     Food food = Food();
+    Snake snake = Snake();
     //game loop
     while(WindowShouldClose() == false)
     {
+        BeginDrawing();
         //1. Event 
         
         //2. Updating postn
         
         //3. Draw
-        BeginDrawing();
+        snake.update();
         ClearBackground(background);
         food.Draw();
+        snake.Draw();
         EndDrawing();
     }
 
